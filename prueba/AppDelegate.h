f9b1df4233691771e72179c999ac90470a272ca0
Vidal Romero <vidal_romero@hotmail.com>
2016-06-06 12:29:38 -0500

    Initial Commit

---
+++ prueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prueba
+//
+//  Created by vidal romero  on 06/06/16.
+//  Copyright © 2016 vidal romero . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ prueba/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  prueba
+//
+//  Created by vidal romero  on 06/06/16.
+//  Copyright © 2016 vidal romero . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
